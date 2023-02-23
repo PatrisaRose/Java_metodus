a31a255acd2856c979954afc0e5263953fc7bc58
PatrsiaRose <cseresznyevirag4@gmail.com>
2023-02-23 18:22:49 +0100

    szünetes feladat

--- Metodusok.java
+++ Metodusok.java
@@ -5,6 +5,15 @@ public class Metodusok {
     
     public static void main(String[] args) {
        kiir("Elsõ 10 szám összege: "+elso10szam()+"\n");
+       
+       int osszeg1, osszeg2, osszeg3;
+       int szam1 = 6, szam2 = 3, szam3 = 8, szam4 = 9;
+       osszeg1 = osszead(szam1, szam2);
+       kiir("%d + %d = %d\n".formatted(szam1, szam2, osszeg1));
+       osszeg2 = osszead(szam3, szam4);
+       kiir("%d + %d = %d\n".formatted(szam3, szam4, osszeg2));
+       osszeg3 = osszead(osszeg1, osszeg2);
+       kiir("%d + %d = %d\n".formatted(osszeg1, osszeg2, osszeg3));
     }
     private static int elso10szam(){
         int osszeg = 0;
